c9e18df1ee3d514f459a1d1ef34d7a78fc934355
ceamattos <carlos@sensio.com.br>
2022-03-09 23:16:00 -0300

    creating deliveryman

---
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import CreateDeliverymanUseCase from "./CreateDeliverymanUseCase";
+
+export default class CreateDeliverymanController {
+  async handle(request: Request, response: Response) {
+    const { username, password } = request.body;
+
+    const createDeliveryUseCase = new CreateDeliverymanUseCase();
+    const result = await createDeliveryUseCase.execute({ username, password });
+
+    return response.json(result);
+  }
+}
