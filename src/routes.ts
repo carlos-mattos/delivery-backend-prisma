f9c5ec7e16ce1c73870c24c2e43e9c9660d88e7b
ceamattos <carlos@sensio.com.br>
2022-03-10 21:33:45 -0300

    creating delivery

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import AuthenticateClientController from "./modules/account/useCases/authenticateClient/AuthenticateClientController";
 import AuthenticateDeliverymanController from "./modules/account/useCases/authenticateDeliveryman/AuthenticateDeliverymanController";
 import CreateClientController from "./modules/clients/useCases/createClient/CreateClientController";
+import CreateDeliveryController from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
 import CreateDeliverymanController from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
@@ -11,11 +12,13 @@ const authenticateDeliverymanController =
   new AuthenticateDeliverymanController();
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
+const createDeliveryController = new CreateDeliveryController();
 
 routes.post("/client/auth", authenticateClientController.handle);
 routes.post("/deliveryman/auth", authenticateDeliverymanController.handle);
 
 routes.post("/client", createClientController.handle);
 routes.post("/deliveryman", createDeliverymanController.handle);
+routes.post("/delivery", createDeliveryController.handle);
 
 export default routes;
